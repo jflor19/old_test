91b0344867b3fba301cde0e0db8d52938bfe7411
jflor19 <111912204+jflor19@users.noreply.github.com>
2022-08-25 14:16:24 -0400

    Minus Constructor

--- ver2/Account.java
+++ ver2/Account.java
@@ -5,12 +5,7 @@ public class Account {
 	// New Comment for balance
 	private double balance;
 	private String name;
-	
-	// Constructor
-	public Account(double balance) {
-		this("Unknown", balance);
-	}
-	
+		
 	public Account(String name, double balance) {
 		this.name = name;
 		this.balance = balance;
